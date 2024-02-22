5448038ae75cb2ddfd7e687d07b9b7c86685ff61
Bruno R V Silva <dev.brunorvs@gmail.com>
2024-02-22 09:29:04 -0300

    estudo part 1

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+// CSS
+import './App.css';
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// data
+import { wordsList } from "./data/words";
+
+// Components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickdWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a randeom word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
+
+  // Starts the secret word game
+  const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create array of latters
+    let wordLetters = word.split("");
+    
+    wordLetters = wordLetters.map((I) => I.toLowerCase());
+
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  };
+
+  // process the letter imput
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  // restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
+  return (
+    <>
+      <div className="app">
+        {gameStage === "start" && <StartScreen startGame={startGame} />}
+        {gameStage === "game" && <Game
+          verifyLetter={verifyLetter} 
+          pickdWord={pickdWord}
+          pickedCategory={pickedCategory}
+          letters={letters} 
+          guessedLetters={guessedLetters} 
+          wrongLetters={wrongLetters} 
+          guesses={guesses} 
+          score={score} />}
+        {gameStage === "end" && <GameOver retry={retry} />}
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,42 @@
+import "./Game.css";
+
+const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score, }) => {
+  return (
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinhe a Palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a plavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        ))}
+      </div>
+      <div className="letterContainer">
+        <p>Tente Adivinhar Uma Letra Da Palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras Já Utilizadas:</p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Game;
